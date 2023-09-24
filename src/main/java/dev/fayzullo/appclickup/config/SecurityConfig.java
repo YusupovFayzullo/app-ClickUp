31343625d74d2108ebcb74883f30668560f1d555
user <yusupovfayzullo100@gmail.com>
2023-09-24 17:37:19 +0500

    commit change

--- src/main/java/dev/fayzullo/appclickup/config/SecurityConfig.java
+++ src/main/java/dev/fayzullo/appclickup/config/SecurityConfig.java
@@ -12,9 +12,5 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfiguration {
 
 
-    public static void main(String[] args) {
-
-
-    }
 
 }
