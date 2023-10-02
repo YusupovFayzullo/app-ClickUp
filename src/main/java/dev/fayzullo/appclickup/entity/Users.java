fbf42515b010382668a5425b61f2bb43840dc2cc
user <yusupovfayzullo100@gmail.com>
2023-10-02 21:48:37 +0500

    commit change

--- src/main/java/dev/fayzullo/appclickup/entity/Users.java
+++ src/main/java/dev/fayzullo/appclickup/entity/Users.java
@@ -69,7 +69,6 @@ public class Users extends AbsUUIDEntity implements UserDetails {
 
     @Override
     public boolean isEnabled() {
-        System.out.println("J");
         return this.enabled;
     }
 }
